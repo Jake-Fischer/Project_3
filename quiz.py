4e75e6471740e5c5e5447ad1cd2889bebb905ed3
Jake-Fischer <jakefischer000@gmail.com>
2021-10-07 14:40:39 -0500

    Implement ForeignKeyFeild in model.py

--- quiz.py
+++ quiz.py
@@ -1,23 +1,25 @@
-from model import Questions, Results
+from model import Question, Result
 import ui
 from datetime import datetime
 import time
 import uuid
 
 
-def get_topics_and_number_of_questions(): # Obtain all topics and the number of questions in each. Return in a dictionary.
-    questions = Questions.select(Questions.category)
-    categories_and_questions = {}
-    for question in questions:
-        if question.category in categories_and_questions:
-            categories_and_questions[question.category] = categories_and_questions[question.category] + 1
+def get_topics_and_number_of_questions():
+    """Obtain all topics and the number of questions in each. Return in a dictionary."""
+    question_categories = Question.select(Question.category)
+    categories_and_question_count = {}
+    # print(Question.select().count()) Testing 
+    for question in question_categories:
+        if question.category in categories_and_question_count:
+            categories_and_question_count[question.category] = categories_and_question_count[question.category] + 1
         else:
-            categories_and_questions[question.category] = 1
-    return categories_and_questions
+            categories_and_question_count[question.category] = 1
+    return categories_and_question_count
 
 
 def get_quiz_questions(topic, number_of_questions): # Obtain a specific number of questions from the database from a given topic. Return question data in a list.
-    topic_questions = Questions.select(Questions.question_id, Questions.category, Questions.question_text, Questions.correct_answer, Questions.wrong_answer1, Questions.wrong_answer2, Questions.wrong_answer3, Questions.difficulty, Questions.points_available).where(Questions.category == topic)
+    topic_questions = Question.select(Question.question_id, Question.category, Question.question_text, Question.correct_answer, Question.wrong_answer1, Question.wrong_answer2, Question.wrong_answer3, Question.difficulty, Question.points_available).where(Question.category == topic)
     quiz_questions = []
     for i in range(number_of_questions):
         question = topic_questions[i]
@@ -58,29 +60,32 @@ def check_user_answer(correct_answer, user_answer):
 
 
 def create_result_record(time_attempted, question_id, user_answer, points_earned, was_correct, unique_id): # Create a record for the question that was answered
-    result = Results(timestamp = time_attempted, question_id = question_id, user_answer = user_answer, points_earned = points_earned, was_correct = was_correct, unique_id = unique_id)
+    result = Result(timestamp = time_attempted, question_id = question_id, user_answer = user_answer, points_earned = points_earned, was_correct = was_correct, unique_id = unique_id)
     result.save()
 
 
 def calculate_quiz_results(unique_id, total_time_taken):
-    uid = unique_id # Changed for clarity
-    tt = total_time_taken # Changed for clarity
+
     number_of_questions_asked = 0
     number_of_questions_correct = 0
     total_points_available = 0
     total_points_earned = 0
     
-    for question_result in Results.select(Results.question_id).where(Results.unique_id == uid): # Select the question_id from all rows where the unique id is the same as the one given to the function
+    # number = Results.select
+
+    
+
+    for question_result in Result.select(Result.question_id).where(Result.unique_id == unique_id): # Select the question_id from all rows where the unique id is the same as the one given to the function
         number_of_questions_asked = number_of_questions_asked + 1 # Add one to question count
-        for question in Questions.select(Questions.points_available).where(Questions.question_id == question_result.question_id): # Select the points available feild from Questions rows where Question.question_id is equal to Results.question_id 
+        for question in Question.select(Question.points_available).where(Question.question_id == question_result.question_id): # Select the points available feild from Questions rows where Question.question_id is equal to Results.question_id 
             total_points_available = total_points_available + question.points_available # Add to total points available
     
-    for question_result in Results.select(Results.points_earned).where(Results.unique_id == uid, Results.was_correct): # Select the points_earned feild from all with the correct unique_id and that were correct
+    for question_result in Result.select(Result.points_earned).where(Result.unique_id == unique_id, Result.was_correct): # Select the points_earned feild from all with the correct unique_id and that were correct
         number_of_questions_correct = number_of_questions_correct + 1 # Add one to the correct count
         total_points_earned = total_points_earned + question_result.points_earned # Add the points earned from the question
         
     score = total_points_earned / total_points_available * 100
 
-    ui.display_quiz_results(tt, number_of_questions_asked, number_of_questions_correct, total_points_available, total_points_earned, score)
+    ui.display_quiz_results(total_time_taken, number_of_questions_asked, number_of_questions_correct, total_points_available, total_points_earned, score)
 
 
